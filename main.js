79aab4568679ced48fc8be8bf6f398193da28a5d
dinixweb <sd4live@gmail.com>
2021-09-26 17:53:52 +0100

    added dotenv, server port, and api prefix

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const express = require('express') 
 
+
 // express server instance
 const server = express()
 
+
+//dotenv config
+require('dotenv/config')
+
+// api prefix 
+const api = process.env.API_PATH
+
 // server port
-const server_port = process.env.PORT || 80
+const server_port = process.env.SERVER_PORT
 
 // morgan class
 const morgan = require('morgan')
@@ -30,6 +38,6 @@ const Base = require('./server/routes/index')
 server.use('', Base)
 
 // entry port
-server.listen(server_port, ()=>{
+server.listen(server_port,  ()=>{
     console.log(`server startd on http://127.0.0.1:${server_port}/`)
 })
